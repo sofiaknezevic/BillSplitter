61a3c982e95cc97070db85b69719ecb6711f6a80
sofiaknezevic <sofia.knezevic@gmail.com>
2017-02-20 19:28:35 -0800

    All views working, calculation method needed

--- BillSplitter/ViewController.m
+++ BillSplitter/ViewController.m
@@ -9,6 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UISlider *amountOfPeople;
+@property (weak, nonatomic) IBOutlet UIButton *calculateBillSplit;
+@property (weak, nonatomic) IBOutlet UILabel *finalBill;
+@property (weak, nonatomic) IBOutlet UILabel *numberOfPeople;
 
 @end
 
@@ -16,7 +21,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
+ 
+}
+- (IBAction)calculateBill:(UIButton *)sender {
+    
+    double bill = [self.billAmount.text floatValue];
+    
+    
+    NSString *finalBillText = [NSString stringWithFormat:@"To split this bill you need to pay $%0.2f each.", bill];
+    
+    self.finalBill.text = finalBillText;
+    
+    
+    
+}
+- (IBAction)calculatePeople:(UISlider *)sender {
+    
+    self.numberOfPeople.text = [NSString stringWithFormat:@"%.0f People", sender.value];
 }
 
 
